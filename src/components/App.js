fc0f312134e9769a77465580542bade8e5eadb59
AllRud <rall@i.ua>
2021-06-06 23:08:13 +0300

    applied ReactRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,73 +1,82 @@
-import React, { Component } from 'react'
+//import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom'
+//import { ListItem } from 'react-native-elements'
 import UserContainer from '../containers/UserContainer'
-import SelectContainer from '../containers/SelectContainer'
 import ButtonContainer from '../containers/ButtonContainer'
 import coursesList from '../utils/courses'
 import modulesList from '../utils/modules'
 import { requestCreate } from '../utils/requests'
 import { requestLoad } from '../utils/requests'
-import { appMode, AppModeContext } from '../components/App-context'
 
 import '../containers/Reset.css'
 import './App.css'
 import './Сhoice-actions.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      appMode,
-    }
-    //let msg
+export default function ParamsExample() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"> Courses</h1>
+          <Switch>
+            <Route path="/:id" children={<Child />} />
+          </Switch>
+          <UserContainer />
+        </header>
+        <div className="Choice-actions">
+          <ul className="topmenu">
+            <li className="down">
+              <span className="label-sel"> Курс</span>
+              <RenderOptions listItems={coursesList} />
+            </li>
 
-    //this.toggleAppMode = () => {
-    //  this.setState(state => ({
-    //     appMode.appCourse:
-    //  }));
-    //};
-  }
+            <li className="down">
+              <span className="label-sel">Модуль:</span>
+              <RenderOptions listItems={modulesList} />
+            </li>
+          </ul>
 
-  componentDidUpdate() {
-    console.log(appMode.appCourse)
-  }
-
-  msg = (<p>Выбран курс {appMode.appCourse}</p>)
-
-  render() {
-    return (
-      <AppModeContext.Provider value={appMode}>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title"> Courses</h1>
-            {this.msg}
-            <UserContainer />
-          </header>
-          <div className="Choice-actions">
-            <SelectContainer
-              selectList={coursesList}
-              labelButton={'Выберите курс:'}
-              id={'coursesList'}
-            />
-            <SelectContainer
-              selectList={modulesList}
-              labelButton={'Выберите модуль:'}
-              id={'modulesList'}
-            />
-            <ButtonContainer
-              n={'Открыть'}
-              req={requestLoad[0]}
-              labelButton={'Открыть модуль'}
-            />
-            <ButtonContainer
-              n={'Новый модуль'}
-              req={requestCreate[0]}
-              labelButton={'Создать модуль'}
-            />
-          </div>
+          <ButtonContainer
+            n={'Открыть модуль'}
+            req={requestLoad[0]}
+            //labelButton={'Открыть модуль'}
+          />
+          <ButtonContainer
+            n={'Новый модуль'}
+            req={requestCreate[0]}
+            //labelButton={'Создать модуль'}
+          />
         </div>
-      </AppModeContext.Provider>
-    )
-  }
+      </div>
+    </Router>
+  )
 }
-App.contextType = AppModeContext
-export default App
+//let selectList = { coursesList }
+//list={ coursesList}
+
+const RenderOptions = ({ listItems }) => (
+  <ul className="submenu">
+    {listItems.map((item) => (
+      <li key={item.toString()}>
+        <Link to={item}> {item}</Link>
+      </li>
+    ))}
+  </ul>
+)
+
+function Child({ list }) {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams()
+  return (
+    <div>
+      <h3> {id}</h3>
+    </div>
+  )
+}
